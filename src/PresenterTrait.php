0bb1155f9ee097a6f5dec9f51d0a9282010eaea3
chapcz <loufek@gmail.com>
2016-12-28 22:16:49 +0100

    Static function should not be abstract.

--- src/PresenterTrait.php
+++ src/PresenterTrait.php
@@ -6,25 +6,13 @@ use Nette\Application\ForbiddenRequestException;
 use Nette\InvalidStateException;
 use Nette\Reflection\ClassType;
 use Nette\Reflection\Method;
-
+/**
+ * @method abstract \Nette\Application\UI\PresenterComponentReflection getReflection()
+ * @method \Nette\Security\User getUser()
+ * @method string getAction($fullyQualified = FALSE)
+ */
 trait PresenterTrait
 {
-
-    /**
-     * @return \Nette\Security\User
-     */
-    abstract public function getUser();
-
-    /**
-     * @return \Nette\Application\UI\PresenterComponentReflection
-     */
-    abstract public static function getReflection();
-
-    /**
-     * Returns current action name.
-     * @return string
-     */
-    abstract public function getAction($fullyQualified = FALSE);
 
     public function checkRequirements($element)
     {
